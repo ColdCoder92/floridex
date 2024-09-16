967bb3988ef526197102be47134e908a66e698d6
Lucas Rodriguez <coldcoder9225@pm.me>
2024-09-16 00:05:00 -0400

    Set Up Project

---
+++ app/src/main/java/com/example/floridex/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.example.floridex.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Orange80 = Color(0xFF745700)
+val Green80 = Color(0xFF3F7240)
+val DeepTeal80 = Color(0xFF005757)
+
+val Orange40 = Color(0xFFFFA500)
+val Green40 = Color(0xFF66BB6A)
+val DeepTeal40 = Color(0xFF008080)
