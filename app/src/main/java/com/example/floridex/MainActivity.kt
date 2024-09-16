4d83d95acdbb3e68eeb42fc4cdd33fdcb8a9f1ac
Lucas Rodriguez <coldcoder9225@pm.me>
2024-09-16 00:06:50 -0400

    Create Description Page Skeleton

---
+++ app/src/main/java/com/example/floridex/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.floridex
+
+import android.R.attr.screenSize
+import android.R.attr.start
+import android.content.Context
+import android.content.res.Resources
+import android.graphics.LinearGradient
+import android.graphics.Shader
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.example.floridex.ui.theme.DeepTeal40
+import com.example.floridex.ui.theme.DeepTeal80
+import com.example.floridex.ui.theme.FloridexTheme
+import com.example.floridex.ui.theme.Green40
+import com.example.floridex.ui.theme.Green80
+import com.example.floridex.ui.theme.Orange40
+import com.example.floridex.ui.theme.Orange80
+import com.example.floridex.Description
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FloridexTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val description = Description()
+                    description.MakeDescription("Cat", modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
