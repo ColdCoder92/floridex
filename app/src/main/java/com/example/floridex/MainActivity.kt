01c3c6aeeef4bd48046ec71f839fec68f1f61ded
Erick Sanchez <sanchez8erick@gmail.com>
2024-11-30 23:01:17 -0500

    Added functionality

--- app/src/main/java/com/example/floridex/MainActivity.kt
+++ app/src/main/java/com/example/floridex/MainActivity.kt
@@ -1,74 +1,186 @@
 package com.example.floridex
 
-import android.R.attr.screenSize
-import android.R.attr.start
-import android.content.Context
-import android.content.res.Resources
-import android.graphics.LinearGradient
-import android.graphics.Shader
 import android.os.Build
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.View
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.annotation.RequiresApi
-import androidx.compose.animation.core.VisibilityThreshold
-import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.TileMode
-import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
-import com.example.floridex.ui.theme.DeepTeal40
-import com.example.floridex.ui.theme.DeepTeal80
-import com.example.floridex.ui.theme.Green40
-import com.example.floridex.ui.theme.Green80
-import com.example.floridex.ui.theme.Orange40
-import com.example.floridex.ui.theme.Orange80
-import com.example.floridex.Description
-import com.example.floridex.CreatureList
-import com.example.floridex.Settings
+import com.example.floridex.databinding.RegisterPageBinding
 
+class MainActivity : ComponentActivity(), View.OnClickListener, View.OnFocusChangeListener, View.OnKeyListener {
 
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.floridex.ui.theme.FloridexTheme
+    private lateinit var mBinding: RegisterPageBinding
 
-class MainActivity : ComponentActivity() {
     @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            FloridexTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    val description = Description()
-                    val settings = Settings()
-                    val appContext = applicationContext
-                    /*
-                    description.MakeDescription(modifier = Modifier.padding(innerPadding),
-                        appContext, 0
-                    )
+        mBinding = RegisterPageBinding.inflate(LayoutInflater.from(this))
+        setContentView(mBinding.root)
+        mBinding.usernameInput.onFocusChangeListener = this
+        mBinding.emailInput.onFocusChangeListener = this
+        mBinding.passwordInput.onFocusChangeListener = this
+        mBinding.confirmPasswordInput.onFocusChangeListener = this
 
-                     */
-                    settings.MakeSettingsMenu(modifier = Modifier.padding(innerPadding), appContext)
+//        enableEdgeToEdge()
+//        setContent {
+//            FloridexTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    val description = Description()
+//                    val settings = Settings()
+//                    val appContext = applicationContext
+//                    /*
+//                    description.MakeDescription(modifier = Modifier.padding(innerPadding),
+//                        appContext, 0
+//                    )
+//
+//                     */
+//                    settings.MakeSettingsMenu(modifier = Modifier.padding(innerPadding), appContext)
+//                }
+//            }
+//        }
+    }
+
+    private fun validateUsername(): Boolean{
+        var errorMessage: String? = null
+        val value: String = mBinding.usernameInput.text.toString()
+        if (value.isEmpty()){
+            errorMessage = "Full name cannot be empty"
+        }
+
+        if (errorMessage != null){
+            mBinding.usernameTIL.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+        }
+
+        return errorMessage == null
+    }
+
+    private fun validateEmail(): Boolean {
+        var errorMessage: String? = null
+        val value: String = mBinding.emailInput.text.toString()
+        if (value.isEmpty()) {
+            errorMessage = "Email cannot be empty"
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(value).matches()) {
+            errorMessage = "Invalid email"
+        }
+
+        if (errorMessage != null){
+            mBinding.emailTIL.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+        }
+
+        return errorMessage == null
+    }
+
+    private fun validatePassword(): Boolean {
+        var errorMessage: String? = null
+        val value: String = mBinding.passwordInput.text.toString()
+        if (value.isEmpty()) {
+            errorMessage = "Password cannot be empty"
+        }
+        else if(value.length < 6){
+            errorMessage = "Password must be longer than 5 characters"
+        }
+
+        if (errorMessage != null){
+            mBinding.passwordTIL.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+        }
+
+        return errorMessage == null
+    }
+
+    private fun validateConfirmPassword(): Boolean {
+        var errorMessage: String? = null
+        val value: String = mBinding.passwordInput.text.toString()
+        if (value.isEmpty()) {
+            errorMessage = "Password cannot be empty"
+        }
+        else if(value.length < 6){
+            errorMessage = "Password must be longer than 5 characters"
+        }
+
+        if (errorMessage != null){
+            mBinding.confirmPasswordTIL.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+
+        return errorMessage == null
+    }
+
+    private fun validatePasswordMatch(): Boolean {
+        var errorMessage: String? = null
+        val password: String = mBinding.passwordInput.text.toString()
+        val confirmPassword: String = mBinding.confirmPasswordInput.text.toString()
+        if (password != confirmPassword) {
+            errorMessage = "Passwords do not match"
+        }
+
+        if (errorMessage != null){
+            mBinding.confirmPasswordTIL.apply {
+                isErrorEnabled = true
+                error = errorMessage
+            }
+
+        return errorMessage == null
+    }
+
+    override fun onClick(view: View?) {
+    }
+
+    override fun onFocusChange(view: View?, hasFocus: Boolean) {
+        if(view != null){
+            when(view.id){
+                R.id.username_input -> {
+                    if(hasFocus){
+                        if(mBinding.usernameTIL.isErrorEnabled){
+                            mBinding.usernameTIL.isErrorEnabled = false
+                        }
+                    }else{
+                        validateUsername()
+                    }
+                }
+                R.id.email_input -> {
+                    if(hasFocus){
+                        if(mBinding.emailTIL.isErrorEnabled){
+                            mBinding.emailTIL.isErrorEnabled = false
+                        }
+                    }else{
+                        validateEmail()
+                    }
+                }
+                R.id.password_input -> {
+                    if(hasFocus){
+                        if(mBinding.passwordTIL.isErrorEnabled){
+                            mBinding.passwordTIL.isErrorEnabled = false
+                        }
+                    }else{
+                        validatePassword()
+                    }
+                }
+                R.id.confirm_password_input -> {
+                    if(hasFocus){
+                        if(mBinding.confirmPasswordTIL.isErrorEnabled){
+                            mBinding.confirmPasswordTIL.isErrorEnabled = false
+                        }
+                    }else{
+                        validateConfirmPassword()
+                    }
                 }
             }
         }
+    }
+
+    override fun onKey(view: View?, event: Int, keyEvent: KeyEvent?): Boolean {
+        return false
     }
 }
--- app/src/main/java/com/example/floridex/RegisterPage.kt
+++ app/src/main/java/com/example/floridex/RegisterPage.kt
@@ -1,2 +1,2 @@
-package com.example.floridex
+ package com.example.floridex
 
